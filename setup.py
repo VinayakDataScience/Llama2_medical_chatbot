7d9c1f723c8cdb5f0d4422cea3c93127d21d696b
VinayakDataScience <vinayak.ganji@gmail.com>
2024-01-07 15:35:37 +0530

    updated files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Vinayak Ganji',
+    author_email= 'vinayak.ganji@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
